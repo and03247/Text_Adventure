f4baf351e1a6ca6e0b5e1d63c13e9c87a1afd132
Elliot Anderson <and03247@umn.edu>
2018-06-16 01:54:31 -0500

    Create Project Files
    This is the descriptiond

---
+++ TextAdventure/Game_Logic/GameLogic.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextAdventure.Game_Logic
+{
+    /// <summary>
+    /// The game logic class.
+    /// </summary>
+    public class GameLogic
+    {
+        public string PlayerName;
+        public Gender Gender;
+
+        public GameLogic(string playerName, Gender gender)
+        {
+            PlayerName = playerName;
+            Gender = gender;
+        }
+
+        public GameLogic()
+        {
+            // This hsould show up in the source control
+        }
+    }
+
+    public enum Gender
+    {
+        Male, Female, NonBinary
+    }
+}
